b09f829f9a7fe1ec34f47634f30fa5bc8cee3c28
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-02-17 19:45:10 +0300

    Created 'X-Sum' by A2SV Hub

---
+++ 17-Feb-2025/X-Sum 269449.py
@@ -0,0 +1,31 @@
+# Problem: X-Sum - https://codeforces.com/problemset/problem/1676/D
+
+t = int(input()) 
+
+for _ in range(t):
+    n, m = map(int, input().split())  
+    board = [list(map(int, input().split())) for _ in range(n)]  
+
+    diag1 = {}  
+    diag2 = {}   
+
+    for r in range(n):
+        for c in range(m):
+            key1 = r - c
+            key2 = r + c
+
+            if key1 not in diag1:
+                diag1[key1] = 0
+            if key2 not in diag2:
+                diag2[key2] = 0
+
+            diag1[key1] += board[r][c]
+            diag2[key2] += board[r][c]
+
+    max_x_sum = 0
+    for r in range(n):
+        for c in range(m):
+            x_sum = diag1[r - c] + diag2[r + c] - board[r][c] 
+            max_x_sum = max(max_x_sum, x_sum)
+
+    print(max_x_sum)
