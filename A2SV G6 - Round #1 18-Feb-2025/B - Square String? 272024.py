a11f075f94df12d3fb718829c47ffba7ff77b8cf
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-02-18 17:09:23 +0300

    Created 'B - Square String?' by A2SV Hub

---
+++ A2SV G6 - Round #1 18-Feb-2025/B - Square String? 272024.py
@@ -0,0 +1,16 @@
+# Problem: B - Square String? - https://codeforces.com/gym/585107/problem/B
+
+from collections import Counter
+t = int(input())
+for _ in range(t):
+    s = input()
+    n = len(s)
+    if n % 2 != 0:
+        print('NO')
+    else:
+        for i in range(n//2):
+            if s[i] != s[n//2+i]:
+                print('NO')
+                break
+        else:
+            print('YES')
