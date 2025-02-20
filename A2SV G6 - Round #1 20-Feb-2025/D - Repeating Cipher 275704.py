701eddf95250bc3e218fc18f9c1ad4841f432b7e
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-02-20 10:09:17 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 20-Feb-2025/D - Repeating Cipher 275704.py
@@ -0,0 +1,11 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n = int(input())
+s = input()
+ans = []
+k, i = 1, 0
+while i < len(s):
+    ans.append(s[i])
+    i += k
+    k += 1
+print(''.join(ans))
