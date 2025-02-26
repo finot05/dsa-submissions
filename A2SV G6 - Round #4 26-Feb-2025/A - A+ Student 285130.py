4d1f83a27df19252b44561df948c1f7dfc88d717
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-02-26 19:22:41 +0300

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/A - A+ Student 285130.py
@@ -0,0 +1,19 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+t = int(input())
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    ans = []
+    if max(b, c)>=a:
+        ans.append(max(b, c) - a + 1)
+    else:
+        ans.append(0)
+    if max(a, c) >= b:
+        ans.append(max(a, c) - b + 1)  
+    else:
+        ans.append(0)
+    if max(a, b) >= c:
+        ans.append(max(a, b) - c + 1)
+    else:
+        ans.append(0)
+    print(*ans)
