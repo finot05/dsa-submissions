439e2d1456d1a7f7f7721737d788e03a74d6a36d
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-03-04 08:09:35 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 04-Mar-2025/B - Fix the Forecast! 290481.py
@@ -0,0 +1,18 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    a = [(i, num) for num, i in enumerate(a)]
+    a.sort()
+    b.sort()
+    # print(a)
+    # print(b)
+    ans = [0] * n
+    for i in range(n):
+        # print(a[i])
+        ans[a[i][1]] = b[i]
+    print(*ans)
