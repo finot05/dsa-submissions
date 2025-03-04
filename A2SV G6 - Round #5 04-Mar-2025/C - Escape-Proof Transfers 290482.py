851f07552ccebc6e814ba663b9b57c85702c4f58
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-03-04 08:09:57 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 04-Mar-2025/C - Escape-Proof Transfers 290482.py
@@ -0,0 +1,14 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+n, t, c = map(int, input().split())
+s = list(map(int, input().split()))
+valid = 0
+count = 0
+for i in range(n):
+    if s[i] <= t:
+        valid += 1
+    else:
+        valid = 0
+    if valid >= c:
+        count += 1
+print(count)
