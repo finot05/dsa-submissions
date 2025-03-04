34af349cc290b10061ae7a156c74ca91d6c60673
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-03-04 08:09:31 +0300

    Created 'A - Language Identifier' by A2SV Hub

---
+++ A2SV G6 - Round #5 04-Mar-2025/A - Language Identifier 290480.py
@@ -0,0 +1,13 @@
+# Problem: A - Language Identifier - https://codeforces.com/gym/591928/problem/A
+
+t = int(input())
+for _ in range(t):
+    s = input()
+    if s[-1] == 'o':
+        print('FILIPINO')
+    elif s[-1] == 'u':
+        print('JAPANESE')
+    else:
+        print('KOREAN')
+
+      
