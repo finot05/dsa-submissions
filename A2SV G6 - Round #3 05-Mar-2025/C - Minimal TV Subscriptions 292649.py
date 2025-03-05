71607d0583c85224a35c956bc082025b856dd43c
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-03-05 11:20:17 +0300

    Created 'C - Minimal TV Subscriptions' by A2SV Hub

---
+++ A2SV G6 - Round #3 05-Mar-2025/C - Minimal TV Subscriptions 292649.py
@@ -0,0 +1,25 @@
+# Problem: C - Minimal TV Subscriptions - https://codeforces.com/gym/588468/problem/C
+
+from collections import Counter
+def min_tv_sub(n, k, d, a):
+    sub_arr = Counter(a[:d])
+    res = len(sub_arr)
+    for i in range(d, n):
+        sub_arr[a[i]] += 1
+        if sub_arr[a[i - d]] > 0:
+            sub_arr[a[i - d]] -= 1
+        if sub_arr[a[i - d]] == 0:
+            del sub_arr[a[i - d]]
+        res = min(res, len(sub_arr))
+    return res
+
+
+
+t = int(input())
+results = []
+for _ in range(t):
+    n, k, d = map(int, input().split())
+    a = list(map(int, input().split()))
+    results.append(str(min_tv_sub(n, k, d, a)))
+
+print('\n'.join(results))
