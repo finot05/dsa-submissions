ba3db2ea249f358e5cec1be35fb1ff12daec226d
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-03-13 18:13:36 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 13-Mar-2025/E - Equalizing Arrays 305139.py
@@ -0,0 +1,27 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+if sum(a) != sum(b):
+    print(-1)
+    exit()
+f, s = 0, 0
+c = 0
+# cur1, cur2 = 0, 0
+while f < n and s < m:
+    if a[f] == b[s]:
+        c += 1
+        f += 1
+        s += 1
+    elif a[f] < b[s]:
+        f += 1
+        if f < n:
+            a[f] += a[f-1]
+    else:
+        s += 1
+        if s < m:
+            b[s] += b[s-1]
+print(c if c != 0 else -1) 
