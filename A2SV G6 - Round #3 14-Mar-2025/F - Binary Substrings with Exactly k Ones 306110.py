5622a8ba4745a89c431e954c185d95461cc83051
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-03-14 08:09:14 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 14-Mar-2025/F - Binary Substrings with Exactly k Ones 306110.py
@@ -0,0 +1,24 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+k = int(input())
+s = input()
+
+
+def atmostk(k):
+    count = 0
+    l = 0
+    ans = 0
+    if k == -1:
+        return 0
+    for r in range(len(s)):
+        
+        if s[r] == '1':
+            count += 1
+        while count > k:
+            if s[l] == '1':
+                count -= 1
+            l += 1
+        ans += r-l+1
+        
+    return ans
+print(atmostk(k) - atmostk(k-1))
