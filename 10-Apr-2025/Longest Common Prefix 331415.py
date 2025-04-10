4a7a6e21b83d67163b4d792a770ba8278a7dd45b
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-04-11 00:02:52 +0300

    Created 'Longest Common Prefix' by A2SV Hub

---
+++ 10-Apr-2025/Longest Common Prefix 331415.py
@@ -0,0 +1,16 @@
+# Problem: Longest Common Prefix - https://leetcode.com/problems/longest-common-prefix/
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+
+        for string in strs[1:]:
+            while not string.startswith(prefix):
+                prefix = prefix[:-1] 
+                if not prefix:  
+                    return ""
+
+        return prefix
