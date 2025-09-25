1187e97bf341cbfc1ab2caf4a33be3e67415847e
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-09-25 12:10:13 +0300

    Created 'A - Abenezer's String Problem' by A2SV Hub

---
+++ A2SV G6 - Round #12 25-Sep-2025/A - Abenezer's String Problem 396143.py
@@ -0,0 +1,9 @@
+# Problem: A - Abenezer's String Problem - https://codeforces.com/gym/606404/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    max_char = sorted(s)[-1]
+    print(ord(max_char) - ord('a')+1)
+    
