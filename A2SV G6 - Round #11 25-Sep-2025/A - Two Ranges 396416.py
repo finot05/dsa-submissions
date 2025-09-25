e89435ca49363686de8ec35dca52813e55cea659
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-09-25 22:38:19 +0300

    Created 'A - Two Ranges' by A2SV Hub

---
+++ A2SV G6 - Round #11 25-Sep-2025/A - Two Ranges 396416.py
@@ -0,0 +1,11 @@
+# Problem: A - Two Ranges - https://codeforces.com/gym/604781/problem/A
+
+q = int(input())
+for _ in range(q):
+    l1, r1, l2, r2 = map(int, input().split())
+    a = l1
+    b = l2
+    if a == b:  
+        b = r2
+    print(a, b)
+    
