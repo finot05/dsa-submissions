9fca9e19ad4a96f50fb7ba79716ea17b12178275
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-09-25 16:34:50 +0300

    Created 'B - Distribute' by A2SV Hub

---
+++ A2SV G6 - Round #12 25-Sep-2025/B - Distribute 396258.py
@@ -0,0 +1,9 @@
+# Problem: B - Distribute - https://codeforces.com/gym/606404/problem/B
+
+n = int(input())
+a = list(map(int, input().split()))
+indx = list(enumerate(a))
+indx.sort(key=lambda x: x[1])
+# print(indx)
+for i in range(n//2):
+    print(indx[i][0]+1, indx[n-i-1][0]+1)
