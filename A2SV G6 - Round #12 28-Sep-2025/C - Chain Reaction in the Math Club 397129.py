0ed645bdde2e5ca28be3847d6332d13acda47202
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-09-28 23:08:50 +0300

    Created 'C - Chain Reaction in the Math Club' by A2SV Hub

---
+++ A2SV G6 - Round #12 28-Sep-2025/C - Chain Reaction in the Math Club 397129.py
@@ -0,0 +1,30 @@
+# Problem: C - Chain Reaction in the Math Club - https://codeforces.com/gym/606404/problem/C
+
+from collections import defaultdict, deque
+
+n, m = map(int, input().split())
+graph = defaultdict(list)
+degree = [0] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    degree[a] += 1
+    degree[b] += 1
+
+ans = 0
+
+while True:
+    q = deque([i for i in range(1, n + 1) if degree[i] == 1])
+    if not q:
+        break 
+    ans += 1
+    while q:
+        node = q.popleft()
+        for nei in graph[node]:
+            if degree[nei] > 0:  
+                degree[nei] -= 1
+        degree[node] = 0  
+
+print(ans)
