2c6a713cbf4b86cf74fc1ecd55f9cb79998ebb89
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-09-29 11:11:14 +0300

    Created 'Min Cost Climbing Stairs' by A2SV Hub

---
+++ 29-Sep-2025/Min Cost Climbing Stairs 397244.py
@@ -0,0 +1,13 @@
+# Problem: Min Cost Climbing Stairs - https://leetcode.com/problems/min-cost-climbing-stairs/
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = {}
+        def dp(i):
+            if i >= len(cost):
+                return 0
+            
+            if i not in memo:
+                memo[i] = min(cost[i] + dp(i+1), cost[i] + dp(i+2))
+            return memo[i]
+        return min(dp(0), dp(1))
