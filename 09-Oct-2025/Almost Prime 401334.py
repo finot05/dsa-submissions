50193073e18556dc13946a2226f29b5b80ec41bf
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-10-09 08:52:42 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 09-Oct-2025/Almost Prime 401334.py
@@ -0,0 +1,23 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+n = int(input())
+
+def is_almost_prime(n):
+    count = 0
+    for x in range(2, n + 1):
+        factors = set()
+        d = 2
+        temp = x
+        while d * d <= temp:
+            if temp % d == 0:
+                factors.add(d)
+                while temp % d == 0:
+                    temp //= d
+            d += 1
+        if temp > 1:
+            factors.add(temp)
+        if len(factors) == 2:
+            count += 1
+    return count
+
+print(is_almost_prime(n))
