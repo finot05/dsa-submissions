9efbcfcf584b7ad7c1f3223db19eb4c7e67614ea
Finoteloza Sisay <167538397+finot05@users.noreply.github.com>
2025-10-16 19:20:31 +0300

    Created 'Word Break' by A2SV Hub

---
+++ 16-Oct-2025/Word Break 403749.py
@@ -0,0 +1,21 @@
+# Problem: Word Break - https://leetcode.com/problems/word-break/description/
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = {}
+        def dfs(i):
+            if i == len(s):
+                return True
+            if i in dp:
+                return dp[i]
+
+            for w in wordDict:
+                if s[i:i+len(w)] == w:
+                    if dfs(i+len(w)):
+                        dp[i] = True
+                        return True
+            
+            dp[i] = False
+            return False
+            
+        return dfs(0)
